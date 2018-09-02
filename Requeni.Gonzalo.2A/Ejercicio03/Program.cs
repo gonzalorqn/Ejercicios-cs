d19013dbfad7bd83d888f3f0138a8e856b5b6390
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2018-09-02 17:54:11 -0300

    Add files via upload

--- Requeni.Gonzalo.2A/Ejercicio03/Program.cs
+++ Requeni.Gonzalo.2A/Ejercicio03/Program.cs
@@ -13,9 +13,32 @@ namespace Ejercicio03
             Console.Title = "Ejercicio Nro 03";
 
             int numero;
+            int primo=0;
 
             Console.Write("Ingrese un numero: ");
             numero = int.Parse(Console.ReadLine());
+            while (numero <= 0)
+            {
+                Console.Write("ERROR. ¡Reingresar número!: ");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            for(int i=1;i<=numero;i++)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    if(i%j == 0)
+                    {
+                        primo++;
+                    }
+                }
+                if(primo == 2)
+                {
+                    Console.WriteLine(i);
+                }
+                primo = 0;
+            }
+            Console.ReadLine();
         }
     }
 }
