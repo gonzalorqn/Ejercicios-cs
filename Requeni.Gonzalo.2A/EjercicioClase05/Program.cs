dfc4547daba1ab3f195380cd4c5217f68cb84d1d
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2018-09-04 12:08:24 -0300

    Add files via upload

---
+++ Requeni.Gonzalo.2A/EjercicioClase05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tinta obj1 = new Tinta();
+            Tinta obj2 = new Tinta(ConsoleColor.Red);
+
+            if(obj1 == obj2)
+            {
+                Console.WriteLine("Son iguales");
+            }
+            else
+            {
+                Console.WriteLine("Son distintos");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Requeni.Gonzalo.2A/EjercicioClase05/Tinta.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase05
+{
+    class Tinta
+    {
+        private ConsoleColor _color;
+        private ETipoTinta _tipo;
+
+        #region Constructores
+        public Tinta()
+        {
+            this._color = ConsoleColor.Blue;
+            this._tipo = ETipoTinta.ConBrillito;
+        }
+
+        public Tinta(ConsoleColor color) : this()
+        {
+            this._color = color;
+        }
+
+        public Tinta(ConsoleColor color, ETipoTinta tipo) : this(color)
+        {
+            this._tipo = tipo;
+        }
+        #endregion
+
+        #region Metodos
+        public static string Mostrar(Tinta obj)
+        {
+            return obj._Mostrar();
+        }
+
+        private string _Mostrar()
+        {
+            return "Color: " + this._color.ToString() + " Tipo: " + this._tipo.ToString();
+        }
+        #endregion
+
+        #region Sobrecarga operadores
+        public static bool operator ==(Tinta a, Tinta b)
+        {
+            bool retorno = false;
+            if(a._tipo == b._tipo && a._color == b._color)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Tinta a, Tinta b)
+        {
+            return !(a == b);
+        }
+        #endregion
+    }
+}
