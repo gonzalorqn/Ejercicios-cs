b0320521afe5c8bf0bd3917c2ed3cbf00e3b3c68
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2018-09-05 12:04:55 -0300

    Add files via upload

--- Requeni.Gonzalo.2A/EjercicioClase05/Program.cs
+++ Requeni.Gonzalo.2A/EjercicioClase05/Program.cs
@@ -12,7 +12,7 @@ namespace EjercicioClase05
         {
             Tinta obj1 = new Tinta();
             Tinta obj2 = new Tinta(ConsoleColor.Red);
-
+            Console.WriteLine(Tinta.Mostrar(obj1));
             if(obj1 == obj2)
             {
                 Console.WriteLine("Son iguales");
@@ -21,6 +21,19 @@ namespace EjercicioClase05
             {
                 Console.WriteLine("Son distintos");
             }
+
+            Pluma obj3 = new Pluma(10,"asd",obj2);
+            Pluma obj4 = new Pluma(10, "ggfg", obj1);
+            Pluma obj5 = new Pluma();
+
+            Console.WriteLine(obj3);
+            Console.WriteLine(obj4);
+            obj3 += obj2;
+            obj4 -= obj1;
+           // obj3 -= obj1;
+            Console.WriteLine(obj3);
+            Console.WriteLine(obj4);
+            Console.WriteLine(obj5);
             Console.ReadLine();
         }
     }
